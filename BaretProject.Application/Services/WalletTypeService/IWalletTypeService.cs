942172baf7ac266a6a369b0051714203bf5138c3
parhamdavin <parhamdavinn3674@gmail.com>
2023-08-28 21:44:04 +0330

    Add Service For WalletType

---
+++ BaretProject.Application/Services/WalletTypeService/IWalletTypeService.cs
@@ -0,0 +1,21 @@
+﻿using BaretProject.Application.DTOs;
+using BaretProject.Application.DTOs.PermissionDTOs;
+using BaretProject.Application.DTOs.WalletType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaretProject.Application.Services.WalletTypeService
+{
+    public interface IWalletTypeService
+    {
+        Task<IEnumerable<WalletTypeDto>> GetAll();
+        Task<WalletTypeDto> GetById(int id);
+        bool IExist(int id);
+        Task<WalletTypeDto> Rigester(WalletTypeDto walletTypeDto);
+        Task Remove(int id);
+        Task<WalletTypeDto> Update(WalletTypeDto walletTypeDto);
+    }
+}
---
+++ BaretProject.Application/Services/WalletTypeService/WalletTypeService.cs
@@ -0,0 +1,75 @@
+﻿using BaretProject.Application.Contracts.Repositories;
+using BaretProject.Application.DTOs;
+using BaretProject.Application.DTOs.WalletType;
+using BaretProject.Application.Extention;
+using BaretProject.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaretProject.Application.Services.WalletTypeService
+{
+    public class WalletTypeService:IWalletTypeService
+    {
+        #region filed
+        private readonly IWalletTypeRepository _repository;
+        public WalletTypeService(IWalletTypeRepository walletTypeRepository)
+        {
+            _repository = walletTypeRepository;
+
+        }
+
+        public async Task<IEnumerable<WalletTypeDto>> GetAll()
+        {
+            var list = await _repository.TableAsNoTracking.Where(W => !W.IsRemoved).Select(W => new WalletTypeDto()
+            {
+                Title = W.Title
+
+            }).ToListAsync();
+            return list;
+        }
+
+        public async Task<WalletTypeDto> GetById(int id)
+        {
+            var wallet = await _repository.FindByIdAsync(id);
+            var WalletDTO = wallet.ToDTO<WalletTypeDto>();
+            return WalletDTO;
+        }
+
+        public bool IExist(int id)
+        {
+            var prod = _repository.FindByIdAsNoTracking(id);
+            if (prod == null) return false;
+            return true;
+        }
+
+        public async Task Remove(int id)
+        {
+            var wallettype = await _repository.FindByIdAsync(id);
+            wallettype.IsRemoved = true;
+            await _repository.UpdateAsync(wallettype);
+        }
+
+        public async Task<WalletTypeDto> Rigester(WalletTypeDto walletTypeDto)
+        {
+            var wallettype = walletTypeDto.ToEntity<WalletType>();
+            await _repository.AddAsync(wallettype);
+            walletTypeDto.ID = wallettype.Id;
+
+            return walletTypeDto;
+        }
+
+        public async Task<WalletTypeDto> Update(WalletTypeDto walletTypeDto)
+        {
+            var wallettype = await _repository.FindByIdAsync(walletTypeDto.ID);
+            wallettype.Title = walletTypeDto.Title;
+            await _repository.UpdateAsync(wallettype);
+            return walletTypeDto;
+        }
+        #endregion
+
+    }
+}

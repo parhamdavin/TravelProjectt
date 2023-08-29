3058222d0d66eaeec0c82fac989ae03c8d8369c1
parhamdavin <parhamdavinn3674@gmail.com>
2023-08-29 13:24:04 +0330

    add ConfigureApplicationServices in the program

--- BaretProject.api/Program.cs
+++ BaretProject.api/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.ConfigureApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
